a9d8252a7f87e22fa573c95bc46f28e047b702e7
Ben Lippmeier <benl@ouroborus.net>
2016-06-02 13:43:33 +1000

    Add section on mapping and folding

--- reference/Flow.hs
+++ reference/Flow.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns, NoMonomorphismRestriction #-}
 
 module Flow where
 import System.IO
@@ -15,7 +16,8 @@ data Sinks   i m e
    , eject  :: i -> m () }
 
 
------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- | Create sources from some named files.
 sourceFs :: [FilePath] -> IO (Sources Int IO Char)
 sourceFs names = do
  hs <- mapM (\n -> openFile n ReadMode) names
@@ -27,6 +29,7 @@ sourceFs names = do
  return (Sources (length names) pulls)
 
 
+-- | Create sinks to some named files.
 sinkFs  :: [FilePath] -> IO (Sinks Int IO Char)
 sinkFs names = do
  hs <- mapM (\n -> openFile n WriteMode) names
@@ -35,7 +38,8 @@ sinkFs names = do
  return (Sinks (length names) pushs ejects)
 
 
------------------------------------------------------------
+-------------------------------------------------------------------------------
+-- | Drain elements from a source to a sink.
 drainP :: Sources Int IO a -> Sinks Int IO a -> IO ()
 drainP (Sources i1 ipull) (Sinks i2 opush oeject) = do
  let drainStream i
@@ -53,7 +57,7 @@ drainP (Sources i1 ipull) (Sinks i2 opush oeject) = do
  mapM_ readMVar mvs
 
 
------------------------------------------------------------
+-------------------------------------------------------------------------------
 dup_ooo :: (Ord i, Monad m)
         => Sinks i m a -> Sinks i m a -> Sinks i m a
 dup_ooo (Sinks n1 push1 eject1) 
@@ -63,9 +67,9 @@ dup_ooo (Sinks n1 push1 eject1)
    in  Sinks (min n1 n2) pushs ejects
 
 
-dup_io :: (Ord i, Monad m)
-       => Sources i m a -> Sinks i m a -> Sources i m a
-dup_io (Sources n1 pull1) 
+dup_ioi :: (Ord i, Monad m)
+        => Sources i m a -> Sinks i m a -> Sources i m a
+dup_ioi (Sources n1 pull1) 
        (Sinks   n2 push2 eject2)
  = let pull3 i eat3 eject3
         = pull1 i eat1 eject1
@@ -74,7 +78,51 @@ dup_io (Sources n1 pull1)
    in  Sources (min n1 n2) pull3
 
 
+-------------------------------------------------------------------------------
+-- | Like `map_i`, but the worker function is also given the stream index.
+map_i   :: (a -> b) -> Sources i m a -> Sources i m b
+map_i f (Sources n pullsA)
+ = Sources n pullsB
+ where  pullsB i eatB ejectB
+         = pullsA i eatA ejectA
+         where  eatA v = eatB (f v)
+                ejectA = ejectB
 
+
+-- | Like `map_o`, but the worker function is also given the stream index.
+map_o  :: (a -> b) -> Sinks i m b -> Sinks i m a
+map_o f (Sinks n pushB ejectB)
+ = Sinks n pushA_map ejectA_map
+ where  pushA_map  i a  = pushB  i (f a)
+        ejectA_map i    = ejectB i
+
+≡
+
+-------------------------------------------------------------------------------
+folds_iii
+        :: (Ord i, Monad m)
+        => (b -> a -> b) -> b
+        -> Sources i m Int 
+        -> Sources i m a 
+        -> Sources i m b
+
+folds_iii f z (Sources nL pullLen) (Sources nX pullX)
+ = Sources (min nL nX) pull_folds
+ where  
+        pull_folds i eat eject
+         = pullLen i eat_len eject_len
+         where 
+               eat_len len = loop_folds len z
+               eject_len   = eject
+                   
+               loop_folds !c !acc
+                | c == 0    = eat acc
+                | otherwise = pullX i eat_x eject_x
+                where eat_x x = loop_folds (c - 1) (f acc x)
+                      eject_x = eject
+
+
+-------------------------------------------------------------------------------
 copyMultipleP 
  :: [FilePath] -> [FilePath] -> [FilePath] -> IO ()
 copyMultipleP srcs dsts1 dsts2
